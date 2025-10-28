a27f535eea1caf51d75b637fa7af8554253de936
Dust472 <dustynfree67@gmail.com>
2025-10-28 15:56:09 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,18 +7,21 @@
 using namespace std;
 using namespace pros;
 
+
+
 Controller master(E_CONTROLLER_MASTER);
 MotorGroup left_mg({1, -2, -3});
 MotorGroup right_mg({11, -12, 13});
-Motor intake(10);
-IMU odomPodActual(19);
-
+MotorGroup intake({-9, 10});
+IMU inertialSensor(19);
+// poopooopooo
+//676776767676767667676767
 class odom {
 	private:
 		MotorGroup* leftDT;
 		MotorGroup* rightDT;
 		double position[2];
-		IMU* odomPodMethod;
+		IMU* inertialSensor;
 		double Kp;
 	public:
 		/*
@@ -26,7 +29,7 @@ class odom {
 		to the respective sides.
 		*/
 		odom() {
-			this->odomPodMethod = &odomPodActual;
+			this->inertialSensor = &inertialSensor;
 			this->leftDT = &left_mg;
 			this->rightDT = &right_mg;
 			this->position[0] = 0;
@@ -94,7 +97,7 @@ void disabled() {}
  * starts.
  */
 void competition_initialize() {
-
+	inertialSensor.reset();
 }
 
 /**
@@ -112,55 +115,6 @@ void autonomous() {
 
 }
 
-
-/**
- * Runs the operator control code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the operator
- * control mode.
- *
- * If no competition control is connected, this function will run immediately
- * following initialize().
- *
- * If the robot is disabled or communications is lost, the
- * operator control task will be stopped. Re-enabling the robot will restart the
- * task, not resume it from where it left off.
- */
-void opcontrol() { 
-	lcd::print(1, "Intake task working");
-	Task movingDT(moveDriveTrain);
-	Task movingIntake(moveIntake);
-}
-
-/**
- * These tasks are nested to allow our intake to be able to move at the same time that our drivetrain moves
- * (that is, the different tasks inside these functions are the drivetrain's logic)
- */
-void moveDriveTrain() {
-	Task bButtonPress(bButtonPressed);
-	Task rightButtonPress(rightButtonPressed);
-	Task leftButtonPress(leftButtonPressed);
-	Task moveMotor(moveMotors);
-}
-
-
-/**
- * This allows moving the drivetrain to be asynchronous with moving the intake
- * by using two different tasks in the opcontrol() function
- */
-void moveIntake() {
-	while(true) {
-		if(master.get_digital(E_CONTROLLER_DIGITAL_L1)) {
-			intake.move_velocity(200);  
-		}
-		else {
-			intake.move_velocity(0);
-		}
-		pros::Task::delay(20);
-	}
-}
-
-
 /*
 These variables are the main part of the logic of the drivetrain
 Allows us to easily swap between two-stick and one stick
@@ -169,13 +123,11 @@ oneOrTwo variable: One = true, two = false
 rightOrLeft variable: Right = true, left = false
 */
 bool rightOrLeft = true;
-bool oneOrTwo = true;
-
+bool oneOrTwo = false;
 
 // Because rightOrLeft will not be constant, we cannot rely on it for when we switch back to the original.
 // So, this variable is made to hold the original value
 bool defaultRight = rightOrLeft;
-
 
 /**
  * This function is its own independent event. It changes the global rightOrLeft value to true if and only if
@@ -222,7 +174,7 @@ void bButtonPressed() {
 	}
 }
 
-// This method is dependent upon the button input from the last 3 methods. 
+// This method is dependent upon the button input from the bButtonPressed(), leftButtonPressed(), and rightButtonPressed() methods
 void moveMotors() {
 	while(true) {
 		int rightStickValueX = master.get_analog(E_CONTROLLER_ANALOG_RIGHT_X);
@@ -230,8 +182,7 @@ void moveMotors() {
 		int leftStickValueX = master.get_analog(E_CONTROLLER_ANALOG_LEFT_X);
 		int leftStickValueY = master.get_analog(E_CONTROLLER_ANALOG_LEFT_Y);
 
-		// This switch statement handles all of the boolean logic
-		// It moves the motors based on this logic
+		// This switch statement handles all of the button logic : it moves our drivetrain based on this logic
 		switch(oneOrTwo) {
 			case true:
 				if(rightOrLeft == true) {
@@ -252,3 +203,140 @@ void moveMotors() {
 		pros::Task::delay(5);
 	}
 }
+
+/**
+ * These tasks are nested to allow our intake to be able to move at the same time that our drivetrain moves
+ * (that is, the different tasks inside these functions are the drivetrain's logic)
+ */
+void moveDriveTrain() {
+	Task bButtonPress(bButtonPressed);
+	Task rightButtonPress(rightButtonPressed);
+	Task leftButtonPress(leftButtonPressed);
+	Task moveMotor(moveMotors);
+}
+
+
+bool isIntaking = false;
+bool isExtaking = false;
+
+/**
+ * This allows moving the drivetrain to be asynchronous with moving the intake
+ * by using two different tasks in the opcontrol() function
+ */
+void moveIntake() {
+	while(true) {
+		if(!isExtaking) {
+			if(master.get_digital(E_CONTROLLER_DIGITAL_L1)) {
+				intake.move_velocity(200);
+				isIntaking = true;
+			}
+			else {
+				intake.move_velocity(0);
+				isIntaking = false;
+			}
+		}
+		pros::Task::delay(20);
+	}
+}
+
+void extake() {
+	while(true) {
+		if(!isIntaking) {
+			if(master.get_digital(E_CONTROLLER_DIGITAL_R1)) {
+				intake.move_velocity(-200);
+				isExtaking = true;
+			}
+			else {
+				intake.move_velocity(0);
+				isExtaking = false;
+			}
+		}
+		pros::Task::delay(20);
+	}
+}
+
+/**
+ * Runs the operator control code. This function will be started in its own task
+ * with the default priority and stack size whenever the robot is enabled via
+ * the Field Management System or the VEX Competition Switch in the operator
+ * control mode.
+ *
+ * If no competition control is connected, this function will run immediately
+ * following initialize().
+ *
+ * If the robot is disabled or communications is lost, the
+ * operator control task will be stopped. Re-enabling the robot will restart the
+ * task, not resume it from where it left off.
+ */
+void opcontrol() { 
+	lcd::print(1, "Intake task working");
+	Task movingDT(moveDriveTrain);
+	Task movingIntake(moveIntake);
+	Task extaking(extake);
+}
+
+/*
+movement type (boolean, true = one stick, false = two stick)
+right stick or left stick (boolean, true = right stick, false = left stick)
+
+if(b button new press) then
+	one or two stick = opposite of its current value
+if(one or two stick == one stick) then
+	if(left button new press) then
+		right stick or left stick = left
+	if(right button new press) then 
+		right stick or left stick = right
+*/
+
+// void opcontrol() {
+// 	bool oneOrTwo = true;
+// 	bool rightOrLeft = true;
+// 	bool initOneOrTwo = oneOrTwo;
+// 	while(true) {
+// 		int turnRight = master.get_analog(E_CONTROLLER_ANALOG_RIGHT_X);
+// 		int dirLeft = master.get_analog(E_CONTROLLER_ANALOG_LEFT_Y);
+// 		int turnLeft = master.get_analog(E_CONTROLLER_ANALOG_LEFT_X);
+// 		int dirRight = master.get_analog(E_CONTROLLER_ANALOG_RIGHT_Y);
+
+// 		if(master.get_digital_new_press(E_CONTROLLER_DIGITAL_B)) {
+// 			oneOrTwo = !oneOrTwo;
+// 			if(oneOrTwo == false) {
+// 				rightOrLeft = initOneOrTwo;
+// 			}
+// 		}
+
+// 		if(oneOrTwo) {
+// 			if(master.get_digital_new_press(E_CONTROLLER_DIGITAL_LEFT)) {
+// 				rightOrLeft = false;
+// 			}
+// 			if(master.get_digital_new_press(E_CONTROLLER_DIGITAL_RIGHT)) {
+// 				rightOrLeft = true;
+// 			}
+// 		}
+
+// 		switch(oneOrTwo) {
+// 			case true:
+// 				if(rightOrLeft == true) {
+// 					right_mg.move_velocity(dirRight - turnRight);
+// 					left_mg.move_velocity(dirRight + turnRight);
+// 				}
+// 				else {
+// 					right_mg.move_velocity(dirLeft - turnLeft);
+// 					left_mg.move_velocity(dirLeft + turnLeft);
+// 				}
+// 				break;
+// 			case false:
+// 				right_mg.move_velocity(dirLeft - turnRight);
+// 				left_mg.move_velocity(dirLeft + turnRight);
+
+// 		}
+// 		pros::Task::delay(5);
+// 	}
+// }
+
+
+
+
+
+
+
