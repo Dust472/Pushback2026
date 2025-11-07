2ee63d3ae4a5b9e132f2de0ffb663ac064df3144
Dust472 <dustynfree67@gmail.com>
2025-11-07 15:57:38 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,73 +7,222 @@
 using namespace std;
 using namespace pros;
 
-#define DIGITAL_SENSOR_PORT 1
-#define DIGITAL_SENSOR_PORT_2 2
 
 
 Controller master(E_CONTROLLER_MASTER);
-MotorGroup left_mg({1, -2, -3});
+MotorGroup left_mg({-1, 2, 3});
 MotorGroup right_mg({11, -12, 13});
-MotorGroup intake({-9, 10});
-IMU inertialSensor(19);
-bool state = LOW;
-pros::adi::Pneumatics intakeHoodPiston1('A', true);
-pros::adi::Pneumatics intakeHoodPiston2('B', false);
+Motor firstStageIntake(10);
+Motor secondStageIntake(-9);
+MotorGroup intake({firstStageIntake.get_port(), secondStageIntake.get_port()});
+Rotation odomP(18);
+IMU inertialSensor(15);
+adi::Pneumatics intakeHoodPiston1('A', false);
+adi::Pneumatics scraperMech1('B', false);
+class odom {
+	private:
+		double position[3];
+		bool odomOn;
+		double leftAverage;
+		double rightAverage;
+		double odomPPosition;
+	public:
+		/*
+		This is the constructor for our odom object. It takes in the sides of the drivetrains and sets the class attributes (above)
+		to the respective sides.
+		*/
+		odom() {
+			this->position[0] = 0;
+			this->position[1] = 0;
+			this->position[2] = 0;
+			this->odomOn = false;
+			this->leftAverage = 0;
+			this->rightAverage = 0;
+			this->odomPPosition = 0;
+		}
+		/**
+		 * This resets the position arrays values. The plan is to use this after we reach each target position. That way, we can get accurate
+		 * feedback for when we check to see if the robot's position is 
+		 */
+		void resetBotPosition() {
+			this->position[0] = 0;
+			this->position[1] = 0;
+		}
 
+		void resetBotOrientation() {
+			this->position[2] = 0;
+		}
 
-// poopooopooo
-//676776767676767667676767
-// class odom {
-// 	private:
-// 		MotorGroup* leftDT;
-// 		MotorGroup* rightDT;
-// 		double position[2];
-// 		IMU* inertialSensor;
-// 		double Kp;
-// 	public:
-// 		/*
-// 		This is the constructor for our odom object. It takes in the sides of the drivetrains and sets the class attributes (above)
-// 		to the respective sides.
-// 		*/
-// 		odom() {
-// 			this->leftDT = &left_mg;
-// 			this->rightDT = &right_mg;
-// 			this->position[0] = 0;
-// 			this->position[1] = 0;
-// 		}
-		
-// 		/** 
-// 		 * This function actually takes input (when we call this function in the autonomous function below) and moves the bot
-// 		 * based on that input
-// 		 * Parameters:
-// 		 * x: the target x-coordinate for the bot
-// 		 * y: the target y-coordinate for the bot
-// 		 * xErr: the bounds allowed for overshoot (x-axis)
-// 		 * yErr: the bounds allowed for overshoot (y-axis)
-// 		 * */
-// 		void changeBotPosition(double x, double y, double xErr, double yErr) {
-// 			double targetRadius = sqrt((x * x) + (y * y));
-// 			double targetAngle = atan2(y, x);
-// 			double currentRadius;
+		bool getOdomOn() {
+			return this->odomOn;
+		}
+
+		void setOdomOn(bool newValue) {
+			this->odomOn = newValue;
+			if(newValue) {
+				resetBotPosition();
+				resetBotOrientation();
+			}
+		}
+
+		double getPoseX() {
+			return this->position[0];
+		}
+
+		double getPoseY() {
+			return this->position[1];
+		}
+
+		double getOrientation() {
+			return this->position[2];
+		}
+
+		void printPose() {
+			lcd::print(5, "%d", this->position[0]);
+			lcd::print(6, "%d", this->position[1]);
+			lcd::print(7, "%d", this->position[2]);
+		}
+
+		int turnTo(double degrees) {
+			if(!this->odomOn) {
+				return 1;
+			}
+			while(0 > degrees) {
+				degrees += 360;
+			}
+			while(degrees > 360){
+				degrees -= 360;
+			}
+			degrees -= 180;
+			double radians = degrees * (M_PI / 180);
+			/*
+			change in theta = change in left - change in right / track width. Change in left = - change in right
+			so change in theta = -2* change in right / track width
+			so change in right = (change in theta * track width) / -2
+			or change in theta = 2 * change in left / track width
+			so change in left = (change in theta * track width) / 2
+			*/
+			double currentOrientation = inertialSensor.get_yaw() * (M_PI / 180);
 			
-// 			while(abs(this->position[0] - x) > xErr && abs(this->position[1] - y) > yErr){
-// 				currentRadius = sqrt((this->position[0] * this->position[0]) + (this->position[1] * this->position[1]));
-				
-// 			}
-// 			resetBotPosition();
-// 		}
+		}
 
-// 		/**
-// 		 * This resets the position arrays values. The plan is to use this after we reach each target position. That way, we can get accurate
-// 		 * feedback for when we check to see if the robot's position is 
-// 		 */
-// 		void resetBotPosition() {
-// 			this->position[0] = 0;
-// 			this->position[1] = 0;
-// 		}
-// };
 
-// odom odomPod();
+		/** 
+		 * This function updates the position of our odom pod
+		 * */
+		int changeOdomPosition() {
+			if(!this->odomOn) {
+				return 1;
+			}
+
+			vector<double> leftSideEncoders = left_mg.get_position_all();
+			vector<double> rightSideEncoders = right_mg.get_position_all();
+
+			// Get the average of the left side of the DT
+			double leftSideCurrentAvg = 0;
+			double leftSum = 0;
+			for(int i = 0; i < leftSideEncoders.size(); i++) {
+				leftSum += leftSideEncoders[i];
+			}
+			leftSideCurrentAvg = leftSum / leftSideEncoders.size();
+
+
+			// Get the average of the right side of the DT
+			double rightSideCurrentAvg = 0;
+			double rightSum = 0;
+			for(int i = 0; i < rightSideEncoders.size(); i++) {
+				rightSum += rightSideEncoders[i];
+			}
+			rightSideCurrentAvg = rightSum / rightSideEncoders.size();
+
+			// Get the change in the odom's position (divided by 100 since get_position() gives centidegrees which is just 100 times a degree)
+			double changeInOdom = ((odomP.get_position() - this->odomPPosition) / 100) * 2 * (M_PI/180);
+
+			// Get the actual differece in arc length of both the left and right sides averages compared to the last iteration
+			double differenceLeft = abs(leftSideCurrentAvg - this->leftAverage) * 3.25 * (M_PI / 180);
+			double differenceRight = abs(rightSideCurrentAvg - this->rightAverage) * 3.25 * (M_PI/180);
+
+			// Using the formula phetal = phetar + deltaL - deltaR/sL + sR, we find our new orientation
+			double currentAbsOrientation = this->position[2] + (differenceLeft - differenceRight)/15.5;
+
+			// Get the actual change in orientation
+			double changeInOrientation = currentAbsOrientation - this->position[2];
+
+			/*
+			This part checks to see if our orientation even changes
+			if it does, then we use a derived formula
+			If it doesn't then we just use one of the sides of the drivetrain to set to our local
+			x and y (these are the x and y's calculated relative to the bot and not the field)
+			*/
+			double localX;
+			double localY;
+			if(changeInOrientation != 0) {
+				localX = 2 * sin(changeInOrientation/2) * (changeInOdom/changeInOrientation);
+				localY = 2 * sin(changeInOrientation/2) * (differenceRight/changeInOrientation + 6.75);
+			} else {
+				localX = changeInOdom;
+				localY = differenceRight;
+			}
+
+			/*
+			The rest of this code converts from local to global position
+			First, we get the average orientation
+			Then we convert to polar cords using trigonometry, subtracting the average orientation from the angle in order to get global poses
+			Then convert back to cartesian
+			We then add these to the position attributes of the class
+			*/
+			double averageOrientation = this->position[2] + changeInOrientation / 2;
+
+			double polarRadius = sqrt(localX * localX + localY * localY);
+			double polarAngle = atan2(localY, localX) - averageOrientation;
+
+			while(0 > polarAngle){
+				polarAngle += M_PI * 2;
+			}
+
+			while(polarAngle > (M_PI * 2)) {
+				polarAngle -= M_PI * 2;
+			}
+
+			double globalX = polarRadius * cos(polarAngle);
+			double globalY = polarRadius * sin(polarAngle);
+
+			this->position[0] += globalX;
+			this->position[1] += globalY;
+			this->position[2] = currentAbsOrientation;
+
+			// Update all of the variables to the current (was previous)
+			this->leftAverage = leftSideCurrentAvg;
+			this->rightAverage = rightSideCurrentAvg;
+			this->odomPPosition = odomP.get_position();
+			return 1;
+		}
+
+		int changeBotPosition(double x, double y, double xErr, double yErr) {
+			if(!this->odomOn) {
+				return 1;
+			}
+			double targetRadius = sqrt(x * x + y * y);
+			double targetAngle = atan2(y, x);
+
+			double minRadiusX = x - xErr;
+			double maxRadiusX = x + xErr;
+			double minRadiusY = y - yErr;
+			double maxRadiusY = y + yErr;
+
+			double minRadius = sqrt(minRadiusX * minRadiusX + minRadiusY * minRadiusY);
+			double maxRadius = sqrt(maxRadiusX * maxRadiusX + maxRadiusY * maxRadiusY);
+
+			return 1;
+		}
+
+		
+
+		
+		
+};
+
+odom odomPod;
 
 /**
  * Runs initialization code. This occurs as soon as the program is started.
@@ -84,6 +233,8 @@ pros::adi::Pneumatics intakeHoodPiston2('B', false);
 void initialize() {
 	lcd::initialize();
 	lcd::print(0, "code uploaded");
+	right_mg.set_encoder_units_all(E_MOTOR_ENCODER_DEGREES);
+	left_mg.set_encoder_units_all(E_MOTOR_ENCODER_DEGREES);
 }
 
 /**
@@ -103,13 +254,17 @@ void disabled() {}
  * starts.
  */
 void competition_initialize() {
-	inertialSensor.reset();
+	odomP.reset();
+	odomPod.setOdomOn(true);
 }
 
 /*While the Odom is deactivated, this code will move the bot forward or backward
   depending on a value between -1 and 1 using move_voltage.
 */
-void preciseMove(int moveDirection){
+int preciseMove(int moveDirection){
+	if(!odomPod.getOdomOn()) {
+		return 1;
+	}
 	if(moveDirection == 1) {
 		right_mg.move_voltage(7000);
 		left_mg.move_voltage(7000);
@@ -122,8 +277,28 @@ void preciseMove(int moveDirection){
 		right_mg.move_voltage(-7000);
 		left_mg.move_voltage(-7000);
 	}
+	return 1;
 }
 
+void testing() {
+	odomPod.setOdomOn(false);
+	while(true) {
+		preciseMove(1);
+		printf("%d", odomPod.getPoseX());
+		printf("%d", odomPod.getPoseY());
+		delay(1000);
+		preciseMove(0);
+		delay(1000);
+	}
+	
+}
+
+void on_auton_1() {
+	while(true) {
+		odomPod.changeOdomPosition();
+		delay(5);
+	}
+}
 /**
  * Runs the user autonomous code. This function will be started in its own task
  * with the default priority and stack size whenever the robot is enabled via
@@ -136,7 +311,12 @@ void preciseMove(int moveDirection){
  * from where it left off.
  */
 void autonomous() {
-
+	Task updateOdom(on_auton_1);
+	Task test(testing);
+	while(true) {
+		odomPod.printPose();
+		delay(20);
+	}
 }
 
 /*
@@ -239,41 +419,20 @@ void moveDriveTrain() {
 	Task moveMotor(moveMotors);
 }
 
-
-bool isIntaking = false;
-bool isExtaking = false;
-
 /**
  * This allows moving the drivetrain to be asynchronous with moving the intake
  * by using two different tasks in the opcontrol() function
  */
 void moveIntake() {
 	while(true) {
-		if(!isExtaking) {
-			if(master.get_digital(E_CONTROLLER_DIGITAL_L1)) {
-				intake.move_velocity(200);
-				isIntaking = true;
-			}
-			else {
-				intake.move_velocity(0);
-				isIntaking = false;
-			}
-		}
-		pros::Task::delay(20);
-	}
-}
-
-void extake() {
-	while(true) {
-		if(!isIntaking) {
-			if(master.get_digital(E_CONTROLLER_DIGITAL_R1)) {
-				intake.move_velocity(-200);
-				isExtaking = true;
-			}
-			else {
-				intake.move_velocity(0);
-				isExtaking = false;
-			}
+		if(master.get_digital(E_CONTROLLER_DIGITAL_L1)) {
+			intake.move_velocity(200);
+		} else if(master.get_digital(E_CONTROLLER_DIGITAL_R1)) {
+			intake.move_velocity(-200);
+		} else if(master.get_digital(E_CONTROLLER_DIGITAL_L2)) {
+			firstStageIntake.move_velocity(200);
+		} else {
+			intake.move_velocity(0);
 		}
 		pros::Task::delay(20);
 	}
@@ -281,8 +440,17 @@ void extake() {
 
 void hoodIntake1() {
 	while(true) {
-		if(master.get_digital_new_press(E_CONTROLLER_DIGITAL_L2)) {
+		if(master.get_digital_new_press(E_CONTROLLER_DIGITAL_R2)) {
 			intakeHoodPiston1.toggle();
+		}
+		delay(5);
+	}
+}
+
+void scraperMechF() {
+	while(true) {
+		if(master.get_digital_new_press(E_CONTROLLER_DIGITAL_A)) {
+			scraperMech1.toggle();
 		}
 		delay(5);
 	}
@@ -305,8 +473,8 @@ void opcontrol() {
 	lcd::print(1, "Intake task working");
 	Task movingDT(moveDriveTrain);
 	Task movingIntake(moveIntake);
-	Task extaking(extake);
 	Task hoodIntake(hoodIntake1);
+	Task scraperMech(scraperMechF);
 }
 
 /*
